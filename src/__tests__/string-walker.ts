49470a520128d744f30d9c0df4702e263f7b1e30
Pontus Östlund <ponost@roxen.com>
2019-09-19 19:44:54 +0200

    First version

---
+++ src/__tests__/string-walker.ts
@@ -0,0 +1,22 @@
+import 'jest'
+import { StringWalker } from '../string-walker'
+
+describe('StringWalker tests', () => {
+  test('Expect position to be zero after instantiation', () => {
+    const s = new StringWalker('test')
+    expect(s.position).toEqual(0)
+  })
+
+  test('Expect length to be correct', () => {
+    const str = 'this is the test'
+    const s = new StringWalker(str)
+    expect(s.length).toEqual(str.length)
+  })
+
+  test('Expect current() to do its stuff', () => {
+    const str = 'this is a string'
+    const s = new StringWalker(str)
+    expect(s.current()).toEqual(str.charCodeAt(s.position))
+    expect(s.currentChar()).toEqual(str.charAt(s.position))
+  })
+})
