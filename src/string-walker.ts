c46a42af0854916b9cb98084adb053a59ec5d4a7
Pontus Östlund <ponost@roxen.com>
2019-09-20 17:40:23 +0200

    Added TSDoc comments

--- src/string-walker.ts
+++ src/string-walker.ts
@@ -1,8 +1,21 @@
+/**
+ * Class with methods to navigates within a string
+ */
 export class StringWalker {
-  protected data: string
+  /** The string to operate on */
+  protected readonly data: string
+  /** The length of the string */
   protected len: number
+  /** Current position within the string */
   protected cursor: number
 
+  /**
+   * Constructor
+   *
+   * @param data The string to operate on
+   * @param normalizeWhiteSpace If `true` newline characters will be normalized
+   * to only consist of `\n`. Default is `false`.
+   */
   constructor(data: string | Buffer, normalizeWhiteSpace = false) {
     if (data instanceof Buffer) {
       data = data.toString('utf8')
@@ -17,18 +30,30 @@ export class StringWalker {
     this.cursor = 0
   }
 
+  /** Property getter for the current position within the string */
   public get position() {
     return this.cursor
   }
 
+  /** Property getter for the length of the string */
   public get length() {
     return this.len
   }
 
+  /** Check if the the cursor is at the end of the string */
   public isEof() {
     return this.cursor >= this.len
   }
 
+  /**
+   * Move the cursor to the next position and the return the character code
+   * of the new position
+   *
+   * @see StringWalker.nextChar()
+   *
+   * @returns The character code of the new position. If the next position is
+   * the end of the string `NaN` is returned
+   */
   public next() {
     if (this.cursor + 1 > this.len) {
       return NaN
@@ -38,6 +63,12 @@ export class StringWalker {
     return this.data.charCodeAt(this.cursor)
   }
 
+  /**
+   * @see StringWalker.next()
+   *
+   * @returns The character at the new position. If the next position is the
+   * end of the string `undefined` is returned
+   */
   public nextChar() {
     if (this.cursor + 1 > this.len) {
       return undefined
@@ -47,14 +78,34 @@ export class StringWalker {
     return this.data.charAt(this.cursor)
   }
 
+  /**
+   * Returns the character code of the current position
+   */
   public current() {
     return this.data.charCodeAt(this.cursor)
   }
 
+  /**
+   * Returns the character of the current position
+   */
   public currentChar() {
     return this.data.charAt(this.cursor)
   }
 
+  /**
+   * Look `n` (default `1`) number of characters ahead of the current position
+   *
+   * @example
+   * const s = new StringWalker('lorem ipsum')
+   * s.peek()   // > 111 (o)
+   * s.peek(2)  // > 114 (r)
+   * s.peek(3)  // > 101 (e)
+   * s.peek(99) // > NaN
+   *
+   * @param n Number of characters to peek
+   * @returns The character code of the peeked positon, or `NaN` if peeking
+   * beyond the string end
+   */
   public peek(n = 1) {
     if (this.cursor + n >= this.len) {
       return NaN
@@ -63,6 +114,11 @@ export class StringWalker {
     return this.data.charCodeAt(this.cursor + n)
   }
 
+  /**
+   * @see StringWalker.peek()
+   * @returns The character of the peeked positon, or `undefined` if peeking
+   * beyond the string end
+   */
   public peekChar(n = 1) {
     if (this.cursor + n >= this.len) {
       return undefined
@@ -71,6 +127,12 @@ export class StringWalker {
     return this.data.charAt(this.cursor + n)
   }
 
+  /**
+   * Look `n` (default `1`) number of characters behind the current position.
+   * @param n Number of characters to look behind
+   * @returns The character code of the look behind character, or `NaN` if
+   * looking before the string start
+   */
   public behind(n = 1) {
     n = n || 1
 
@@ -81,6 +143,11 @@ export class StringWalker {
     return this.data.charCodeAt(this.cursor - n)
   }
 
+  /**
+   * @see StringWalker.behind()
+   * @returns The character code of the look behind character, or `NaN` if
+   * looking before the string start
+   */
   public behindChar(n = 1) {
     n = n || 1
 
@@ -91,6 +158,19 @@ export class StringWalker {
     return this.data.charAt(this.cursor - n)
   }
 
+  /**
+   * Find the position of `char`
+   *
+   * @note This method does not move the position within the string
+   *
+   * @example
+   * const s = new StringWalker('lorem ipsum')
+   * s.findNext(' ') // > 4
+   *
+   * @see StringWalker.findNextOf()
+   * @param char Either a character or a character code
+   * @returns The position of `char`, or `NaN` if `char` was not found
+   */
   public findNext(char: string | number) {
     if (typeof char === 'string') {
       if (char.length > 1) {
@@ -115,6 +195,20 @@ export class StringWalker {
     return NaN
   }
 
+  /**
+   * Find the next position of either of `chars`
+   *
+   * @see StringWalker.findNext()
+   *
+   * @example
+   * const s = new StringWalker('lorem ipsum')
+   * s.findNextOf(['s', 'e']) // > 3 (position of e)
+   *
+   * @param chars List of characters to search for. This can either be
+   * characters or character codes
+   * @returns The position of the first found character, or `NaN` if non was
+   * found
+   */
   public findNextOf(chars: Array<string | number>) {
     const x = chars.map((c) => {
       if (typeof c === 'string') {
@@ -143,6 +237,12 @@ export class StringWalker {
     return NaN
   }
 
+  /**
+   * Move the internal cursor by `steps` number of positions
+   * @param steps Steps to move the cursor by
+   * @throws An error if the cursor will move beyond the end, or ahead of
+   * the start, of the string
+   */
   public moveBy(steps: number) {
     this.assertSaneStart(this.cursor + steps)
     this.assertSaneEnd(this.cursor + steps)
@@ -150,6 +250,12 @@ export class StringWalker {
     return this
   }
 
+  /**
+   * Move the internal cursor to the postion `to`
+   * @param to Position to move the cursor to
+   * @throws An error if the cursor will move beyond the end, or ahead of
+   * the start, of the string
+   */
   public moveTo(to: number) {
     this.assertSaneStart(to)
     this.assertSaneEnd(to)
@@ -157,14 +263,34 @@ export class StringWalker {
     return this
   }
 
+  /**
+   * Returns the character code of the character at position `pos`
+   * @param pos The position to get the character code for
+   */
   public at(pos: number) {
     return this.data.charCodeAt(pos)
   }
 
+  /**
+   * Returns the character at position `pos`
+   * @param pos The position to get the character of
+   */
   public charAt(pos: number) {
     return this.data.charAt(pos)
   }
 
+  /**
+   * Move the internal cursor forward as long as any character in `char` is
+   * consecutively found
+   * @example
+   * // Consume all consecutive whitespace
+   * const s = new StringWalker('lorem \t\n ipsum')
+   * const spacePos = s.findNext(' ')
+   * s.moveTo(spacePos).consume([' ', '\n', '\t'])
+   * // Now the cursor is at 'i'.
+   *
+   * @param char A character, character code or array of these
+   */
   public consume(char: string | number | string[] | number[]) {
     if (!Array.isArray(char)) {
       if (typeof char === 'string') {
@@ -191,11 +317,23 @@ export class StringWalker {
     return this
   }
 
+  /**
+   * Reset the internal cursor to position `0`
+   */
   public rewind() {
     this.cursor = 0
     return this
   }
 
+  /**
+   * Returns the substring from `from` to `to`. This behaves the same as
+   * JavaScripts native `substring()`
+   *
+   * @throws An error is throws if either `from` is greater than `to`, from
+   * is less than `0` or `to` is greater than the string length
+   *
+   * @returns The substring of `from` to `to`
+   */
   public substring(from: number, to?: number) {
     if (!to) {
       to = this.length
@@ -211,12 +349,20 @@ export class StringWalker {
     return this.data.substring(from, to)
   }
 
+  /**
+   * Throws an error if `n` is less than `0`
+   * @param n
+   */
   protected assertSaneStart(n: number) {
     if (n < 0) {
       throw new Error(`Start position ${n} is less than zero`)
     }
   }
 
+  /**
+   * Throws an error if `n` is greater than {@see StringWalker#length}
+   * @param n
+   */
   protected assertSaneEnd(n: number) {
     if (n > this.len) {
       throw new Error(`End position ${n} is greater than the string length ${
