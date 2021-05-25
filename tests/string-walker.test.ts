ccfb0171212a5a9462ae090fc5aa4d8d9af2fbbc
Pontus Östlund <ponost@roxen.com>
2021-05-25 11:18:09 +0200

    Minor test fixes

--- tests/string-walker.test.ts
+++ tests/string-walker.test.ts
@@ -1,21 +1,25 @@
 import 'jest'
 import { StringWalker } from '../src/string-walker'
 
+function sw(s: string) {
+  return new StringWalker(s)
+}
+
 describe('StringWalker tests', () => {
   test('Expect position to be zero after instantiation', () => {
-    const s = new StringWalker('test')
+    const s = sw('test')
     expect(s.position).toEqual(0)
   })
 
-  test('Expect length to be correct', () => {
+  test('Expect length to equal the length of the string given to the constructor', () => {
     const str = 'this is the test'
-    const s = new StringWalker(str)
+    const s = sw(str)
     expect(s.length).toEqual(str.length)
   })
 
   test('Expect current() to do its stuff', () => {
     const str = 'this is a string'
-    const s = new StringWalker(str)
+    const s = sw(str)
     expect(s.current()).toEqual(str.charCodeAt(s.position))
     expect(s.currentChar()).toEqual(str.charAt(s.position))
   })
